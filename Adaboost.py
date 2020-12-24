6f92706be3ea076846d2ef8184fc8e02224e33c0
Netanel Indik <netanel117@gmail.com>
2020-12-24 12:51:28 +0200

    adaboost: almost finished rules combinations error computing. main: reorganized

--- Adaboost.py
+++ Adaboost.py
@@ -1,10 +1,33 @@
 """## Adaboost Algorithm"""
 import math
-
 import numpy as np
+from sklearn.model_selection import train_test_split
 
-maximum_weight = 30
+MAX_WEIGHT = 30
 
+
+def df_to_points(features, labels):
+    np_features = features.to_numpy()
+    np_labels = labels.to_numpy()
+    points = []
+    for f, l in zip(np_features, np_labels):
+        p = Point(f[0], f[1], l)
+        points.append(p)
+    return points
+
+
+def rules_from_points(points_list):
+    new_rules = []
+    points = points_list
+    for p1 in points:
+        for p2 in points:
+            if p1.x != p2.x or p1.y != p2.y:
+                rule = Rule(p1, p2, "up")
+                new_rules.append(rule)
+                rule = Rule(p1, p2, "down")
+                new_rules.append(rule)
+        points.remove(p1)
+    return new_rules
 
 # input : points with labels (points list), rules list
 # output : weights of rules (list)
@@ -29,15 +52,37 @@ maximum_weight = 30
 # TODO: we need to make sure that a line wont be considered twice (two sides of same line is ok)
 #######################################
 
-def run(points, adaboost_rules):
+
+def voting(best_rules, k, weighted_point):
+    aihix_sum = 0
+    for i in range(k):
+        aihix_sum += best_rules[i]["weight"] * best_rules[i]["rule"].classify(weighted_point["point"])
+    return aihix_sum
+
+
+def get_empirical_error_on_test(best_rules, k, test_points):
+    pass
+
+
+def get_true_error_on_training(best_rules, k, train_points):
+    pass
+
+
+def run(features_df, labels_df):
+    # split the data set randomly
+    train_x, test_x, train_y, test_y = train_test_split(features_df, labels_df, test_size=0.5)
+    train_points = df_to_points(train_x, train_y)
+    test_points = df_to_points(test_x, test_y)
+    all_possible_rules = rules_from_points(train_points)
+
     # Initialize point weights 𝐷_𝑡 (𝑥_𝑖 )=1/𝑛
-    initial_point_weight = 1 / len(points)
+    initial_point_weight = 1 / len(train_points)
 
-    # list of lists [point, weight]
-    weighted_points = [{"point": p, "weight": initial_point_weight} for p in points]
+    # list of dictionaries {point, weight}
+    weighted_points = [{"point": p, "weight": initial_point_weight} for p in train_points]
 
-    # list of lists [rule, error, weight]
-    w_e_rules = [{"rule": rule, "error": 0, "weight": 0} for rule in adaboost_rules]
+    # list of dictionaries {rule, error, weight}
+    w_e_rules = [{"rule": rule, "error": 0, "weight": 0} for rule in all_possible_rules]
 
     best_rules = []
 
@@ -53,7 +98,7 @@ def run(points, adaboost_rules):
 
             if rule["error"] < minimal_error_rule["weight"]:
                 minimal_error_rule = rule
-
+        # TODO: is the second rule necessary? if the mana is 0 its ok I think
         # update the weight of the minimal error rule and save it to best rules
         if (minimal_error_rule["error"]) != 0 and ((1 - minimal_error_rule["error"]) /
                                                    minimal_error_rule["error"] > 0):
@@ -61,7 +106,7 @@ def run(points, adaboost_rules):
             minimal_error_rule["weight"] = (1 / 2) * np.log((1 - minimal_error_rule["error"]) /
                                                             minimal_error_rule["error"])
         else:
-            minimal_error_rule["weight"] = maximum_weight  # TODO: risky
+            minimal_error_rule["weight"] = MAX_WEIGHT  # TODO: risky
         best_rules.append(minimal_error_rule)
 
         # update all points weights
@@ -71,10 +116,74 @@ def run(points, adaboost_rules):
             p["weight"] = (1 / z) * p["weight"] * math.pow(math.e, (-minimal_error_rule["weight"] *
                                                                     minimal_error_rule["rule"].classify(p["point"]) *
                                                                     p["point"].type))
+
+    # at this point we have the list of 8 best rules after one adaboost run
     # TODO: this function should return list of 8 errors:
-    #  first error - first rule error
-    #   second error - the error of two first rules
-    #   third error - the error of 3 first rules
+    #  first error - first rule error on test set
+    #   second error - the error of two first rules on test set
+    #   third error - the error of 3 first rules on test set
     #   etc..
+    #   do the computing here
+    #
+    hkx_stats = [{"empirical_error_on_test": 0, "true_error_on_training": 0} for k in range(8)]
+    for k in range(8):
+        hkx_stats[k]["empirical_error_on_test"] = get_empirical_error_on_test(best_rules, k, test_points)
+        hkx_stats[k]["true_error_on_training"] = get_true_error_on_training(best_rules, k, train_points)
+
+    # returning stats of one adaboost run
+    # TODO: at the main - run 100 adaboost runs. sum all stats of each rules combination.
+    # after 100 runs, divide all stats by 100. and wev got our results!
+    # do it for both data sets
     ##
-    return best_rules
+    return hkx_stats
+
+
+# def run(points, adaboost_rules):
+#     # Initialize point weights 𝐷_𝑡 (𝑥_𝑖 )=1/𝑛
+#     initial_point_weight = 1 / len(points)
+#
+#     # list of lists [point, weight]
+#     weighted_points = [{"point": p, "weight": initial_point_weight} for p in points]
+#
+#     # list of lists [rule, error, weight]
+#     w_e_rules = [{"rule": rule, "error": 0, "weight": 0} for rule in adaboost_rules]
+#
+#     best_rules = []
+#
+#     k = 8  # number of iterations
+#     z = 0  # sum the points weights
+#     for i in range(k):
+#         minimal_error_rule = w_e_rules[0]
+#         for rule in w_e_rules:
+#             for p in weighted_points:
+#                 # return 1 if wrong, else 0
+#                 if not rule["rule"].classify_is_correct(p["point"]):
+#                     rule["error"] += p["weight"]
+#
+#             if rule["error"] < minimal_error_rule["weight"]:
+#                 minimal_error_rule = rule
+#
+#         # update the weight of the minimal error rule and save it to best rules
+#         if (minimal_error_rule["error"]) != 0 and ((1 - minimal_error_rule["error"]) /
+#                                                    minimal_error_rule["error"] > 0):
+#
+#             minimal_error_rule["weight"] = (1 / 2) * np.log((1 - minimal_error_rule["error"]) /
+#                                                             minimal_error_rule["error"])
+#         else:
+#             minimal_error_rule["weight"] = MAX_WEIGHT  # TODO: risky
+#         best_rules.append(minimal_error_rule)
+#
+#         # update all points weights
+#         for p in weighted_points:
+#             z += p["weight"]
+#         for p in weighted_points:
+#             p["weight"] = (1 / z) * p["weight"] * math.pow(math.e, (-minimal_error_rule["weight"] *
+#                                                                     minimal_error_rule["rule"].classify(p["point"]) *
+#                                                                     p["point"].type))
+#     # TODO: this function should return list of 8 errors:
+#     #  first error - first rule error
+#     #   second error - the error of two first rules
+#     #   third error - the error of 3 first rules
+#     #   etc..
+#     ##
+#     return best_rules
--- main.py
+++ main.py
@@ -2,90 +2,11 @@
 # 311148902
 # 311300784
 
-
-# Load libraries
-
 import pandas as pd
-from sklearn.model_selection import train_test_split
-
 import Adaboost
 from Point import Point
 from Rule import Rule
 
-pd.set_option('display.max_columns', None)
-
-"""## Data preparation"""
-# importing body_temperature dataset
-body_temperature = pd.read_table("HC_Body_Temperature.txt", sep='    ', names=["temp", "gender", "heart_rate"],
-                                 engine='python', encoding="ISO-8859-1")
-
-# reformat gender column: female = -1, male = 1
-body_temperature.gender = body_temperature.gender.apply(lambda x: -1 if x == 1 else 1)
-
-# importing iris dataset
-iris = pd.read_csv("iris.data", names=["sepal_length", "sepal_width", "petal_length", "petal_width", "iris_type"],
-                   encoding="ISO-8859-1", low_memory=False)
-
-# dropping the undesired data from table. we only interested in 2nd and 3rd columns as features
-iris = iris.drop(labels=["sepal_length", "petal_width"], axis=1)
-
-# ignore Iris-setosa rows
-iris = iris.loc[iris.iris_type != "Iris-setosa"]
-
-# reformat iris_type column: Iris-versicolor = -1, Iris-virginica = 1
-iris.iris_type = iris.iris_type.apply(lambda x: -1 if x == "Iris-versicolor" else 1)
-iris.head()
-
-# all datasets are currently pandas.dataframe
-
-# splitting iris to train and test
-iris_Y = iris.iris_type
-# print(iris_Y.shape)
-iris_X = iris.drop(labels="iris_type", axis=1)
-
-# 50% training and 50% test
-iris_X_train, iris_X_test, iris_Y_train, iris_Y_test = train_test_split(iris_X, iris_Y, test_size=0.5)
-
-
-# splitting each of the sets to x (features) and y (label)
-
-
-def ds_to_points(features, labels):
-    np_features = features.to_numpy()
-    np_labels = labels.to_numpy()
-    points = []
-    for f, l in zip(np_features, np_labels):
-        p = Point(f[0], f[1], l)
-        points.append(p)
-    return points
-
-
-def rules_from_points(points_list):
-    new_rules = []
-    points = points_list
-    for p1 in points:
-        for p2 in points:
-            if p1.x != p2.x or p1.y != p2.y:
-                rule = Rule(p1, p2, "up")
-                new_rules.append(rule)
-                rule = Rule(p1, p2, "down")
-                new_rules.append(rule)
-        points.remove(p1)
-    return new_rules
-
-
-"""## Iris"""
-
-# convert iris dataset to points
-
-print("iris_X_train.shape:")
-print(iris_X_train.shape)
-print("iris_Y_train.shape:")
-print(iris_Y_train.shape)
-print("iris x train head:")
-print(iris_X_train.head)
-print("iris y train head:")
-print(iris_Y_train.head)
 
 # TODO: extract data from files
 # TODO: for each dataset:
@@ -97,23 +18,93 @@ print(iris_Y_train.head)
 #           for k in range(8):
 #               compute the empirical error of the function Hk on the test set
 #               compute the true error of Hk on the training set
-#  #
+#
 
-"""# Test"""
-iris_train_points = ds_to_points(iris_X_train, iris_Y_train)
-print("number of iris_train_points:", len(iris_train_points))
-print("points list:")
-for p in iris_train_points:
-    p.print_p()
-rules = rules_from_points(iris_train_points)
-print("number of rules:", len(rules))
-for r in rules:
-    r.print_r()
 
-adaboost_result = Adaboost.run(iris_train_points, rules)
+def main():
+    pd.set_option('display.max_columns', None)
 
-print("\n Results:")
+    """## Data preparation"""
+    # importing body_temperature dataset
+    body_temperature = pd.read_table("HC_Body_Temperature.txt", sep='    ', names=["temp", "gender", "heart_rate"],
+                                     engine='python', encoding="ISO-8859-1")
 
-for r in adaboost_result:
-    r["rule"].print_r()
-    print(r["weight"])
+    # reformat gender column: female = -1, male = 1
+    body_temperature.gender = body_temperature.gender.apply(lambda x: -1 if x == 1 else 1)
+
+    # importing iris dataset
+    iris = pd.read_csv("iris.data", names=["sepal_length", "sepal_width", "petal_length", "petal_width", "iris_type"],
+                       encoding="ISO-8859-1", low_memory=False)
+
+    # dropping the undesired data from table. we only interested in 2nd and 3rd columns as features
+    iris = iris.drop(labels=["sepal_length", "petal_width"], axis=1)
+
+    # ignore Iris-setosa rows
+    iris = iris.loc[iris.iris_type != "Iris-setosa"]
+
+    # reformat iris_type column: Iris-versicolor = -1, Iris-virginica = 1
+    iris.iris_type = iris.iris_type.apply(lambda x: -1 if x == "Iris-versicolor" else 1)
+    iris.head()
+    # all datasets are currently pandas.dataframe
+
+    # splitting iris to features and labels
+    iris_y = iris.iris_type
+    # print(iris_y.shape)
+    iris_x = iris.drop(labels="iris_type", axis=1)
+
+    # TODO: separate body_temp to features and labels
+
+    ################## at this point iris and body_temp should be clean and separated to features and labels ##################
+
+    #single adaboost run.
+    # input: features panda dataframe, labels panda dataframe
+    # output: list of 8 best rules with errors and weights
+    single_run_best_rules = Adaboost.run(iris_x, iris_y)
+
+
+
+
+
+
+    ######### Tests ########
+    # # 50% training and 50% test
+    # iris_X_train, iris_X_test, iris_Y_train, iris_Y_test = train_test_split(iris_x, iris_y, test_size=0.5)
+    #
+    # # splitting each of the sets to x (features) and y (label)
+    # """## Iris"""
+    #
+    # # convert iris dataset to points
+    #
+    # print("iris_X_train.shape:")
+    # print(iris_X_train.shape)
+    # print("iris_Y_train.shape:")
+    # print(iris_Y_train.shape)
+    # print("iris x train head:")
+    # print(iris_X_train.head)
+    # print("iris y train head:")
+    # print(iris_Y_train.head)
+    #
+    # iris_train_points = ds_to_points(iris_X_train, iris_Y_train)
+    # print("number of iris_train_points:", len(iris_train_points))
+    # print("points list:")
+    # for p in iris_train_points:
+    #     p.print_p()
+    # rules = rules_from_points(iris_train_points)
+    # print("number of rules:", len(rules))
+    # for r in rules:
+    #     r.print_r()
+    #
+    # adaboost_result = Adaboost.run(iris_train_points, rules)
+    #
+    # print("\n Results:")
+    #
+    # for r in adaboost_result:
+    #     r["rule"].print_r()
+    #     print(r["weight"])
+    #
+
+
+if __name__ == "__main__":
+    main()
+
+
